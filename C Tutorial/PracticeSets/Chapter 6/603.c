8ce7faeef825f683900c61ed7bb69e0597eefd1f
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-10-22 18:53:39 +0530

    Update 603.c

--- C Tutorial/PracticeSets/Chapter 6/603.c
+++ C Tutorial/PracticeSets/Chapter 6/603.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
  int multiply(int*);
  int multiply(int* x){
-    return *x * 10;
+    *x =*x * 10;
+    
  }
 
  int main(){
     int i = 10;
     printf("Value of i before: %d\n", i);
-    
+    multiply(&i);
 
-    printf("Value of i after: %d\n", multiply(&i));
+    printf("Value of i after: %d\n", i);
 
     return 0;
  }
