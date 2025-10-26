411374d35e9b6d7c68636530916323ccaa038575
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-10-27 01:49:16 +0530

    Add program demonstrating dynamic memory allocation using malloc and realloc

---
+++ C/PracticeSets/Chapter 11/1104.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n = 5;
+    int *ptr;
+
+    ptr = (int *)malloc(n * sizeof(int));
+
+    ptr[0] = 34;
+    ptr[1] = 77;
+    ptr[2] = 21;
+    ptr[3] = 69;
+    ptr[4] = 35;
+
+    printf("%d %d %d %d %d", ptr[0], ptr[1], ptr[2], ptr[3], ptr[4]);
+    ptr = (int*)realloc(ptr, 10* sizeof(int));
+
+    return 0;
+}
