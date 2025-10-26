add202ec22ab09a015dcce2f62dd70be0dc42b52
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-10-26 15:13:44 +0530

    Problem 10: typedef implementation in problem 9

---
+++ C/PracticeSets/Chapter 9/910.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct date
+{
+    int mm;
+    int dd;
+    int yyyy;
+}dt;
+// To compare the dates
+int compare(dt d1, dt d2)
+{
+    if ((d1.yyyy == d2.yyyy) && (d1.mm == d2.mm) && (d1.dd == d2.dd))
+    {
+        return 0;
+    }
+    // If d1.year is in the year return 1:
+    if (d1.yyyy > d2.yyyy)
+    {
+        return 1;
+    }
+    if (d1.yyyy < d2.yyyy)
+    {
+        return -1;
+    }
+    else if (d1.mm > d2.mm)
+    {
+        return 1;
+    }
+    else if (d1.mm < d2.mm)
+    {
+        return -1;
+    }
+    else if (d1.dd > d2.dd)
+    {
+        return 1;
+    }
+    else if (d1.dd < d2.dd)
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    dt d1 = {01, 10, 2006};
+    dt d2 = {26, 9, 2025};
+    printf("%d", compare(d1, d2));
+    printf("\n");
+    printf(" 1 -> d1 is ahead\n");
+    printf("-1 -> is ahead\n");
+    printf(" 0 -> equal dates \n");
+
+    return 0;
+}
