48bab4c758f85c0599ebafe122aafd41e4df0f73
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-10-27 01:45:35 +0530

    Add examples for dynamic memory allocation using free and realloc

---
+++ C/C Tutorial/Chapter 11/04_free.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n ;
+    int *ptr;
+    scanf("%d", &n);
+    ptr = (int *)calloc(n,sizeof(int));
+    
+        ptr[0] = 34;
+        ptr[1] = 77;
+        ptr[2] = 21;
+        ptr[3] = 69;
+        
+        
+        printf("%d %d %d %d ", ptr[0],ptr[1],ptr[2],ptr[3]);
+        free(ptr);
+    return 0; 
+}
