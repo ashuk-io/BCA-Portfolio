5776b5c28b0651d3cd1263c6e9ba5852fc3e5716
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-10-27 23:46:16 +0530

    Homework Set Problem 1

---
+++ C/PracticeSets/HomeworkSet/01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     printf("Enter length of the rectangle : \n");
+//     scanf("%d", &a);
+//     printf("Enter Width of the rectangle : \n");
+//     scanf("%d", &b);
+//     printf("Perimeter of the rectangle = %d", 2 * (a + b));//Returns the value
+//     return 0;
+// }
+int main()
+{
+    int a;
+    printf("Enter a integer for its cube : \n");
+    scanf("%d", &a);//Input from the user
+    printf("Cube = %d", a * a * a);
+    return 0;
+}
