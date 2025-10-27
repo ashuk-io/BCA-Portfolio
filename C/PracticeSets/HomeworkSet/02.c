8289d0aebe7ccae5d79905d078717933c2be70e3
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-10-27 23:46:32 +0530

    Homework Set problem 2

---
+++ C/PracticeSets/HomeworkSet/02.c
@@ -0,0 +1,44 @@
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a = 21;
+//     int b = 35;
+//     int c = 77;
+    
+//     printf("Average = %.2f", (a+b+c)/2.0);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//     char ch;
+
+//     printf("Enter a character: ");
+//     scanf("%c", &ch);
+
+//     // Check if character is between '0' and '9'
+//     if (ch >= '0' && ch <= '9') {
+//         printf("'%c' is a digit.\n", ch);
+//     } else {
+//         printf("'%c' is not a digit.\n", ch);
+//     }
+
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    int a = 35;
+    int b = 42;
+    if(a>b){
+        printf("a is greater");
+        
+    }
+    else{
+        printf("b is greater");
+    }
+    return 0;
+    }
