f37100e6a4ffb6e2bea85a1157086eb9c396cee6
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-11-01 23:25:59 +0530

    Set 3-b: If the given number is a natural number.

---
+++ C/PracticeSets/HomeworkSet/03b.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int num ; 
+    printf("Enter a number : \n");
+    scanf("%d",&num);
+    if(num>0){
+        printf("The given is a natural number \n");
+
+        
+    }
+    else{
+        printf("The given numebr is not a natural number ");
+
+    }
+    return 0;
+}
