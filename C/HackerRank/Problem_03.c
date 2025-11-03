da8910ad8346dde389f507cc421edd4efee17e1e
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-11-03 22:04:12 +0530

    Add Problem 03: Program to perform basic arithmetic operations on integers and floats

---
+++ C/HackerRank/Problem_03.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main(){
+    int a;
+    int b;
+    float c;
+    float d;
+    scanf("%d %d",&a, &b);
+  
+    scanf("%f %f",&c, &d);
+    
+
+    printf("%d %d\n", a+b, a-b);
+    printf("%.1f %.1f\n", c+d, c-d);
+
+    return 0;
+}
